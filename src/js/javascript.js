d6c68923f05548ba01f5ce16158420db2d176452
Roman Kovtun <roman.kovtun@webtomed.com>
2018-03-16 13:40:14 -0500

    removed grunt and moved to gulp

--- src/js/javascript.js
+++ src/js/javascript.js
@@ -1,20 +1,3 @@
-/*
-  TODO: UI MODULE
-    1. get input value
-    2. add new item to ui
-    3. update ui
- */
-
-/*
-   TODO: DATA MODULE
-    1. add new item to data structure
-    2. calculate budget
- */
-
-/*
-   TODO: Control Module
-    1. Add event handler
- */
 
 /*
  *  Budget Controller
